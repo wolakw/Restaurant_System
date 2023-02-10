baf1eb1227dde398ff0bd8468333106a8ecdfef6
wolakw <wojtek.2017w@gmail.com>
2023-02-10 01:34:29 +0100

    final commit

--- src/main/java/com/example/bikeservice/Application.java
+++ src/main/java/com/example/bikeservice/Application.java
@@ -18,7 +18,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
 public class Application implements AppShellConfigurator {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
